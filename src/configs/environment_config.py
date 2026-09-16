0d1d0b143c0416e4bde5f5f022286f859ede7d58
BrendenChouinard <chouinard.b@ufl.edu>
2026-09-15 20:44:44 -0400

    updated observation space to use config and update tests

--- src/configs/environment_config.py
+++ src/configs/environment_config.py
@@ -3,10 +3,11 @@ from dataclasses import dataclass
 @dataclass
 class EnvironmentConfig:
     max_steps: int = 200
-    starting_hp: int = 100
+    max_hp: int = 100
 
 @dataclass
 class MapConfig:
     width: int = 8
     height: int = 8
+    max_elevation: int = 10
     seed: int = 0
